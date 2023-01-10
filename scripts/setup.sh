a23c478afa6ed9a96ba4d4d89acb6465349f687f
trieudao215 <85700300+trieudao215@users.noreply.github.com>
2023-01-10 12:34:07 +0700

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -36,10 +36,7 @@ setuid 65535
 flush
 auth none
 
-users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
-
 $(awk -F "/" '{print "auth none\n" \
-"allow " $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
